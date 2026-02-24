333024f7f9659568408e88510818e1d39a12b01a
Long <lg.t.n.175@outlook.de>
2026-02-24 04:51:34 +0100

    refactor: different static stats for enemies (mummy, slime etc)

--- enemies/slime/slime.h
+++ enemies/slime/slime.h
@@ -1,11 +1,15 @@
 #ifndef SLIME_H
 #define SLIME_H
 
-#include "../enemy.h"
+#include "../melee.h"
 #include <SDL.h>
 
+#define SLIME_MAX_HEALTH 30
+#define MELEE_ATTACK_RANGE 25
+#define SLIME_DAMAGE 10
+
 typedef struct Slime {
-    Enemy base;
+    MeleeEnemy base;
 } Slime;
 
 Slime slime_init(SDL_Renderer *renderer, int x, int y);
